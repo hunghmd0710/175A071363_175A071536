56e7d082e658a0719bc7776a0cd86a59309990a6
hunghmd0710 <57883153+hunghmd0710@users.noreply.github.com>
2019-12-25 17:48:40 +0700

    Add files via upload

---
+++ help.php
@@ -0,0 +1,22 @@
+
+<?php
+    require_once "../config/config.php";  
+    $pageTile = 'Đăng nhập';
+?>
+<!DOCTYPE html>
+<html lang="en"><head>
+	<?php
+    require_once "../includes/inc_head.php";
+    ?>
+</head>
+<body >
+	
+<?php
+    require_once "../includes/inc_header.php";
+    require_once "../includes/inc_help_main.php";
+    require_once "../includes/inc_footer.php";
+    ?>
+
+
+</body>
+</html>
